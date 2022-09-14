4067efff606a75366a866011b9cd6cb20fe2c260
Aishwarya  MG <113001803+AishwaryaGuruprasad@users.noreply.github.com>
2022-09-14 05:43:36 +0000

    done

---
+++ set01/problem01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  printf("Hi! I'm Aishwarya\n");
+  return 0;
+}
---
+++ set01/problem02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{ int a;
+  int b;
+
+  printf("Enter value of a\n");
+  scanf("%d",&a);
+  printf("Enter value of b\n");
+  scanf("%d",&b);
+  printf("The sum of two numbers is: %d",a+b);
+  return 0;
+}
