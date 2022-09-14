a86a31d8fc57425d34a2b2864c3c944a3c274f5d
Aishwarya  MG <113001803+AishwaryaGuruprasad@users.noreply.github.com>
2022-09-14 10:01:38 +0000

    upto problem 6 done

---
+++ set01/problem05.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("Enter the number\n");
+  scanf("%d",&a);
+  return a;
+}
+int largest(int a,int b,int c,)
+{
+  if(a>b && a>c)
+    return a;
+  else if(b>a && b>c)
+    return b;
+  else 
+    return c;
+}
+
+int main()
+{
+  int e,f,g,h;
+  e=input();
+  f=input();
+  g=input();
+  h=largest(e,f,g);
+  printf("Largest number amongst %d ,%d and %d is %d",e,f,g,h);
+  
+  return 0;
+}
