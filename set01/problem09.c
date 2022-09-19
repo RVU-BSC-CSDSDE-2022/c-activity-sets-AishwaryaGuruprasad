99225c5113b8b3845d7d1dbbf4eb700d6a95d736
Aishwarya  MG <113001803+AishwaryaGuruprasad@users.noreply.github.com>
2022-09-19 15:58:44 +0000

    tried problem 9

---
+++ set01/problem09.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<math.h>
+
+float input()
+{
+  float n;
+  printf("Enter the number\n");
+  scanf("%d",&n);
+  return n;
+}
+float square_root(float n)
+{
+  float sqrroot;
+  sqrroot =sqrt(n);
+  return sqrroot;
+}
+void output(float n, float sqrroot)
+{
+  printf("The squareroot of %d is %d ", n, sqrroot);
+}
+float main()
+{
+  float n, sqr;
+  n=input();
+  sqr=square_root(n);
+  output(n,sqr);
+}
