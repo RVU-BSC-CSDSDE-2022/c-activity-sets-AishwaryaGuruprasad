da2d866cf82a9f4e5ce2c48498c66c3c889ee5cf
Aishwarya  MG <113001803+AishwaryaGuruprasad@users.noreply.github.com>
2022-11-16 14:12:49 +0000

    Problem 1 Set 3 done

--- set02/problem07.c
+++ set02/problem07.c
@@ -4,32 +4,37 @@ void input(char *name)
 {
 printf("Enter Camel's name:");
 scanf("%s",name);
-
 }
-int has_nice_name(int count)
+int has_nice_name(char *name)
 { 
-  int n,i;
+  int n,i,count1,count2,flag;
   n=strlen(name);
   for(i=0;i<n;i++)
-    {
-      if(name[i]=="a"||name[i]=="e"||name[i]=="i"||name[i]=="0"||name[i]=="u")
+    { 
+      if(name[i]=='a'|| name[i]=='e'||name[i]=='i' ||name[i]=='o'|| name[i]=='u'||name[i]=='A'|| name[i]=='E'||name[i]=='I' ||name[i]=='O'|| name[i]=='U')
       {count1++;}
-      else
+      else 
       {count2++;}
+      
     }
-  return count;
+    if(count1>=2 && count2>=2)
+    {flag=1;}
+    else 
+    {flag=0;}
+  return flag;
 }
-void output(int res)
+void output(int flag)
 {
-  if(count1>=2 && count2>=2)
+  if(flag==1)
   printf("Camel has a nice name.");
-  else
+  else if(flag==0)
     printf("Camel does not have a nice name");
 }
 int main()
-{
+{ int flag;
   char name[100]; 
-  input(&name);
+  input(name);
+  flag=has_nice_name(&name);
   
-  
-}
+  output(flag);
+ }
