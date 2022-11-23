503e42bfaa8f9ec9ffa30604bdca5bcd2d560c74
Aishwarya  MG <113001803+AishwaryaGuruprasad@users.noreply.github.com>
2022-11-23 06:59:48 +0000

    problem 4 set 4 done

--- set04/problem04.c
+++ set04/problem04.c
@@ -1,32 +1,35 @@
 #include<stdio.h>
+
 int input()
 { int n ;
- printf("Enter n");
- scanf("%d",n); 
+ printf("Enter n:");
+ scanf("%d",&n); 
+ return n;
 }
+
 int find_fibo(int n)
 {
-    int t1,t2,fibo;
-    t1=0;t2=1;
-    for (i = 3; i <= n; ++i) 
+    int t1,t2,fibo,i;
+    t1=0;t2=1,fibo=2;
+    for (i = 2; i <= n; i++) 
     {
-    printf("%d, ",fibo);
+    fibo = t1 + t2;
     t1 = t2;
     t2 = fibo;
-    fibo = t1 + t2;
-      }
-    
+    }
   return fibo;
 }
-/*void output(int n, int fibo)
+
+void output(int fibo)
 {
-  for(int i=0;i<n;i++)
-    printf("%d",a[i]);
-}*/
+printf("%d",fibo);
+}
+
 int main()
 {
   int n,fibo;
   n=input();
   fibo=find_fibo(n);
-  
+  output(fibo);
+  return 0;
 }
