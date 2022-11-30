6bd4659de2547929ba87fb54e2d86bad05cedaa2
Aishwarya  MG <113001803+AishwaryaGuruprasad@users.noreply.github.com>
2022-11-30 06:40:37 +0000

    Set 3 Problem 8 done

--- set03/problem08.c
+++ set03/problem08.c
@@ -4,7 +4,6 @@ typedef struct _triangle
 {
 	float base, height, area;
 } Triangle;
-
 int input_n()
 {
   int n;
@@ -26,7 +25,7 @@ void input_n_triangles(int n, Triangle t[n])
     t[i] = input_triangle();
   }
 }
-void find_area(Traingle *t)
+void find_area(Triangle *t)
 {
   t->area=(t->base*t->height)/2;
 }
@@ -39,10 +38,33 @@ void find_n_areas(int n, Triangle t[n])
 }
 Triangle find_smallest_triangle(int n, Triangle t[n])
 {
-  
+  Triangle smallest=t[0];
+  for(int i=1;i<n;i++)
+    {
+      if(t[i].area<smallest.area)
+      {
+        smallest=t[i];
+      }
+    }
+  return smallest;
 }
 void output(int n, Triangle t[n], Triangle smallest)
 {
-  
+  printf("The smallest of the triangle with base and height");
+  for(int i=0;i<n;i++)
+    {
+      printf("(%.2f,%.2f),",t[i].base,t[i].height);
+    }
+  printf("is the triangle with the base %.2f and height %.2f and area % .2f ", smallest.base,smallest.height,smallest.area);
 }
-
+int main()
+{
+  Triangle s;
+  int n;
+  Triangle t[n];
+  n=input_n();
+  input_n_triangles(n,t);
+  find_n_areas(n,t);
+  s=find_smallest_triangle(n,t);
+  output(n,t,s);
+}
