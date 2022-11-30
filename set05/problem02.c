4573524e774ae0e592208861c58ccc4cd1e3acbf
Aishwarya  MG <113001803+AishwaryaGuruprasad@users.noreply.github.com>
2022-11-30 04:50:25 +0000

    Program 2 done

--- set05/problem02.c
+++ set05/problem02.c
@@ -1,52 +1,52 @@
 #include <stdio.h>
 typedef struct
 {
-    int num, den;
-} Fraction;
-void input(Fraction *f)
-{
- 
-  printf("Enter the numerator and denomenator: \n");
-	scanf("%d %d",&f->num,&f->den);
-	
+    float num, den;
+} 
+Fraction;
+
+Fraction input()
+{ Fraction f;
+    printf("Enter the numerator and denomenator: \n");
+	scanf("%f %f",&f.num,&f.den);
+	return f;
 }
 int smallest(Fraction *f1, Fraction *f2, Fraction *f3)
-{ int result;
-  int r1,r2,r3;
-  r1=f1->num/f1->den;
-   r2=f2->num/f2->den;
-   r3=f3->num/f3->den;
-  if(r1<r2 && r1<r3)
-  { result=-1;}
-  else if(r2<r1 && r2<r3)
-   { result=0;} 
-  else
+{  int result;
+   float fac1,fac2,fac3;
+   fac1= f1->num/f1->den;
+   fac2= f2->num/f2->den;
+   fac3= f3->num/f3->den;
+   if(fac1<fac2 && fac1<fac3)
+  {
+    result=-1;
+    printf("The smallest of %0.1f/%0.1f, %0.1f/%0.1f and %0.1f/%0.1f is %0.1f/%0.1f",f1->num,f1->den,f2->num,f2->den,f3->num,f3->den,f1->num,f1->den);
+  }
+  else if(fac2<fac1 && fac2<fac3)
+  {
+    result=0;
+     printf("The smallest of %0.1f/%0.1f, %0.1f/%0.1f and %0.1f/%0.1f is %0.1f/%0.1f",f1->num,f1->den,f2->num,f2->den,f3->num,f3->den,f2->num,f2->den);
+  }
+  else if(fac3<fac1 && fac3<fac2)
   {
     result=1;
+    printf("The smallest of %0.1f/%0.1f, %0.1f/%0.1f and %0.1f/%0.1f is %0.1f/%0.1f",f1->num,f1->den,f2->num,f2->den,f3->num,f3->den,f3->num,f3->den);
+    
   }
+  else
+  {
+      printf("Error");
+  }
+  
+  printf("%d",result);
   return result;
-}
-void output(Fraction *f1, Fraction *f2, Fraction *f3,int result)
-{
-  if(result==-1)
-  {
-  printf("The smallest of %d/%d, %d/%d and %d/%d is %d/%d",f1->num,f1->den,f2->num,f2->den,f3->num,f3->den,f1->num,f1->den);
-  }
-   if(result==0)
-  {
-  printf("The smallest of %d/%d, %d/%d and %d/%d is %d/%d",f1->num,f1->den,f2->num,f2->den,f3->num,f3->den,f2->num,f2->den);
-  }
-   if(result==1)
-  {
-  printf("The smallest of %d/%d, %d/%d and %d/%d is %d/%d",f1->num,f1->den,f2->num,f2->den,f3->num,f3->den,f3->num,f3->den);
-  }
 }
 int main()
 {
   Fraction f1,f2,f3;int s;
-  input(&f1);
-  input(&f2);
-  input(&f3);
+  f1=input();
+  f2=input();
+  f3=input();
   s=smallest(&f1,&f2,&f3);
-  output(&f1,&f2,&f3,&s);
+  
 }
